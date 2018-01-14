1358f6e2174f582b4d7421468f7b45fddd05347f
Handy Lim <18360062+handylim@users.noreply.github.com>
2018-01-14 19:19:30 +0700

    Add the core function to convert ε-NFA to DFA
    The core function to convert ε-NFA to DFA is completed.
    
    Its input is:
    1. transition_table which is a ε-NFA transition table in Python dict() returned from read_file() function
    2. alphabet which is the set of alphabet to be transitioned with
    3. current_state which is the current state of the process. It's the start state if it's the state marked with ->
    4. result which is dict() whose keys are start_state, final_state, alphabet, and result. This parameter is needed to make the recursive process of the function works
    
    Its output is the Python dict() of DFA transitions, the alphabets, start state, and final state(s)

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def remove_space(str):
 	return ''.join([x for x in str if x is not ' ' and x is not '\t'])
 
 
+def change(set):
+	return str(sorted(set)).replace('[', '{').replace(']', '}').replace('\'', '')
+
+
 """
 	Take all states between '{' and '}' and put it into list to be iterated later
 	Set the alphabet and the states ascendingly
@@ -52,3 +56,30 @@ def read_file(path, alphabet):
 		e_nfa_table.update({'start_state': start_state,
 		                    'final_state': final_state})
 		return e_nfa_table
+
+
+"""
+	Convert the E-NFA to DFA from the transition table recursively
+"""
+def convert(transition_table, alphabet, current_state, result):
+	for a in alphabet:
+		set_union = set()
+		for s in current_state:
+			set_union |= transition_table[s][a]
+		eclose = set()
+		for e in set_union:
+			eclose |= transition_table[e]['epsilon']
+
+		current_state_string = change(current_state)
+		eclose_string = change(eclose)
+
+		if eclose & transition_table['final_state']:
+			result['final_state'] = result['final_state'].union([eclose_string])
+
+		if current_state_string not in result['result']:
+			result['result'].update({current_state_string: {}})
+		result['result'][current_state_string].update({a: eclose_string})
+
+		if eclose_string not in result['result']:
+			convert(transition_table, alphabet, eclose, result)
+	return result
